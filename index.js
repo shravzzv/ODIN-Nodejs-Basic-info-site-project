7d7edc1d7803d2b5823ca0c2fee6ec524c402602
sai shravan <saishravan384@gmail.com>
2024-04-12 09:27:38 +0530

    refactor: Use express

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
-const http = require('http')
+const express = require('express')
 const fs = require('fs')
 
-http
-  .createServer((req, res) => {
-    const path = req.url === '/' ? './index.html' : `.${req.url}.html`
+const app = express()
 
-    fs.readFile(path, 'utf-8', (err, data) => {
-      if (err) {
-        res.writeHead(404, { 'Content-Type': 'text/html' })
-        fs.readFile('./404.html', (_, data) => res.end(data))
-      } else {
-        res.writeHead(200, { 'Content-Type': 'text/html' })
-        res.end(data)
-      }
-    })
+const getResponse = (res, path) =>
+  fs.readFile(path, 'utf-8', (err, data) => {
+    if (err) {
+      res.writeHead(404, { 'Content-Type': 'text/html' })
+      fs.readFile('./404.html', (_, data) => res.end(data))
+    } else {
+      res.writeHead(200, { 'Content-Type': 'text/html' })
+      res.end(data)
+    }
   })
-  .listen(3000, () => console.log('server running on http://localhost:3000'))
+
+app.get('/', (req, res) => getResponse(res, './index.html'))
+app.get('/about', (req, res) => getResponse(res, './about.html'))
+app.get('/contact-me', (req, res) => getResponse(res, './contact-me.html'))
+
+app.listen(3000, () => console.log('server running on http://localhost:3000'))
--- readme.md
+++ readme.md
@@ -1,5 +1,5 @@
 # Node.js Basic Informational Site
 
-This project is the submitted solution to the project ['Basic Informational Site'](https://www.theodinproject.com/lessons/nodejs-basic-informational-site) from The Odin Project.
+This project is the submitted solution to the project [Basic Informational Site](https://www.theodinproject.com/lessons/nodejs-basic-informational-site) from The Odin Project.
 
-The project is a very basic informational site that contains 4 pages: index, about, contact-me and 404. It was created using the node core modules `http` and `fs`.
+The project is a very basic informational site that contains 4 pages: index, about, contact-me and 404. It was created using the `express` node framework and the node core module `fs`.
